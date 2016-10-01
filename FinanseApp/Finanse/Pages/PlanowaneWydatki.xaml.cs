1080dc1d8e0ea784e89c441ac7d1f9778374459f
kumalg <kumalgfilms@gmail.com>
2016-10-01 18:28:03 +0200

    Test nowego menu

--- FinanseApp/Finanse/Pages/PlanowaneWydatki.xaml.cs
+++ FinanseApp/Finanse/Pages/PlanowaneWydatki.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Finanse.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -19,9 +21,89 @@ namespace Finanse.Pages {
 
     public sealed partial class PlanowaneWydatki : Page {
 
+        private ObservableCollection<GroupInfoList<Operation>> _source;
+        private FontFamily iconStyle = new FontFamily(Settings.GetActualIconStyle());
+
+        decimal actualMoney;
+        int actualMonth;
+        int actualYear;
+
         public PlanowaneWydatki() {
 
             this.InitializeComponent();
+
+            actualMonth = DateTime.Today.Month;
+            actualYear = DateTime.Today.Year;
+
+            _source = (new StoreData()).GetGroupsByDay(9, actualYear);
+
+            ContactsCVS.Source = _source;
+
+            foreach (var group in _source) {
+                actualMoney += group.decimalCost;
+            }
+        }
+
+        private void OperacjeListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
+        }
+
+        private void Grid_RightTapped(object sender, RightTappedRoutedEventArgs e) {
+
+        }
+
+        private void Grid_DragStarting(UIElement sender, DragStartingEventArgs args) {
+
+        }
+
+        private void DetailsButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ListView_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args) {
+
+        }
+
+        private void AppBarToggleButton_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void OperationsCommandBarButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e) {
+            MoreAppBarRadioButton.IsChecked = false;
+            if (CommandBar.IsOpen) {
+                OperationsAppBarRadioButton.Height = 40;
+                CategoriesAppBarRadioButton.Height = 40;
+                AddNewOperationAppBarRadioButton.Height = 40;
+                StatisticsAppBarRadioButton.Height = 40;
+                CommandBar.IsOpen = false;
+            }
+            else {
+                OperationsAppBarRadioButton.Height = Double.NaN;
+                CategoriesAppBarRadioButton.Height = Double.NaN;
+                AddNewOperationAppBarRadioButton.Height = Double.NaN;
+                StatisticsAppBarRadioButton.Height = Double.NaN;
+                CommandBar.IsOpen = true;
+            }
+        }
+
+        private void AddNewOperationAppBarRadioButton_Click(object sender, RoutedEventArgs e) {
+            AddNewOperationAppBarRadioButton.IsChecked = false;
         }
     }
 }
